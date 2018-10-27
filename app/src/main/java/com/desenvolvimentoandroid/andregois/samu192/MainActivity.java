569192c2b2b032c11226bfd0238704f4bf7800ba
Andre Gois <andre-gois@hotmail.com>
2018-10-26 23:30:15 -0300

    jjjj

--- app/src/main/java/com/desenvolvimentoandroid/andregois/samu192/MainActivity.java
+++ app/src/main/java/com/desenvolvimentoandroid/andregois/samu192/MainActivity.java
@@ -1,5 +1,6 @@
 package com.desenvolvimentoandroid.andregois.samu192;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +84,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+
+            startActivity(new Intent(MainActivity.this, OcorrenciaActivity.class));
 
         } else if (id == R.id.nav_slideshow) {
 
